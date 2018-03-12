f51a65ec03ea5908cfa73b9acd7d82da1c3bd039
Your name <stanlick@gmail.com>
2018-03-12 14:07:02 -0500

    added global error trap (I thought this happened last week?)

--- src/main/java/com/lambazon/controller/LambazonExceptionHandlerController.java
+++ src/main/java/com/lambazon/controller/LambazonExceptionHandlerController.java
@@ -1,16 +1,29 @@
 package com.lambazon.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+
+@Controller
 @ControllerAdvice
-public class ExceptionHandlerController {
+
+public class LambazonExceptionHandlerController {
 
 	public static final String DEFAULT_ERROR_VIEW = "exception";
 
 	@ExceptionHandler(value = { Exception.class, RuntimeException.class, Error.class })
 	public ModelAndView defaultErrorHandler(Exception e) {
+		ModelAndView mav = new ModelAndView(DEFAULT_ERROR_VIEW);
+		mav.addObject("clue", e.getMessage());
+		return mav;
+	}
+	
+	@GetMapping("/error")
+	public ModelAndView defaultErrorMapping(Exception e) {
 		ModelAndView mav = new ModelAndView(DEFAULT_ERROR_VIEW);
 		mav.addObject("clue", e.getMessage());
 		return mav;
